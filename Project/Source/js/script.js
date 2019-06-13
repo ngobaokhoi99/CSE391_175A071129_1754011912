8515c3f8838067798495640c39d7728792bdc813
anvilz1 <anville1999@gmail.com>
2019-06-14 02:59:31 +0700

    admin

--- Project/Source/js/script.js
+++ Project/Source/js/script.js
@@ -1,8 +1,28 @@
+// Login
 $('#login').click(function() {
         if ($('#name').val()=='admin' && $('#pass').val()=='admin') {
-            window.location.href='';
+            window.location.href='admin.html';
         }
         else{
             $( ".alert-danger" ).show();
         }
+});
+// Thêm mới
+$('#them').click(function() {
+    $('#addNew').removeClass('hidden');
+});
+
+$("#cancel1").click(function() {
+    $("#addNew").addClass('hidden');
+});
+
+//Sửa
+$("#sua").click(function() {
+    $('#repair').removeClass('hidden');
+});
+$('#timkiem').click(function() {
+    $('#old').removeClass('hidden');
+});
+$("#cancel2").click(function() {
+    $("#repair").addClass('hidden');
 });
