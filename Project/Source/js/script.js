debb2211dcd6b7e2d0930e86b228fb2a93b5a405
anvilz1 <anville1999@gmail.com>
2019-06-13 22:29:35 +0700

    login

--- Project/Source/js/script.js
+++ Project/Source/js/script.js
@@ -1,22 +1,8 @@
-  $( document).ready(function()) {
-    $( "#dialog" ).dialog({
-    	autoOpen:false,
-    	show:{
-    		effect:"blind",
-    		duration:1000
-    	},
-    	hide:{
-    		effect:"explode",
-    		duration:1000
-    	},
-    	modal:true,
-    	buttons:{
-    		"Đăng nhập": function(){
-    			alert('Test đăng nhập');
-    		},
-    	}
-    	    "Hủy": function(){
-    	    	alert('Hủy đăng nhập'):
-    	    }
-    	}
-};
+$('#login').click(function() {
+        if ($('#name').val()=='admin' && $('#pass').val()=='admin') {
+            window.location.href='';
+        }
+        else{
+            $( ".alert-danger" ).show();
+        }
+});
